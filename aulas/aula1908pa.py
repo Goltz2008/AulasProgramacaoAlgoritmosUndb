930ae9c315a12c6cbc7bb11d9b62ce31d6dc91cb
Goltz2008 <claudemirgoulart70@gmail.com>
2026-08-19 19:27:43 +0000

    aula1908: variaves e operadores

---
+++ aulas/aula1908pa.py
@@ -0,0 +1,11 @@
+num1 = 4
+num2 = 2
+soma = num1 + num2
+subtracao = num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+
+print("Soma dos numeros: ", soma)
+print("Subtracao dos numeros", subtracao)
+print("Divisao dos numeros", divisao)
+print("Multiplicacao dos numeroa", multiplicacao)
